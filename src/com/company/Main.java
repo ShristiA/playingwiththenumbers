ed1b8fee550a7f06bc0840968b6160e23a67e1ce
ShristiA <shristiamatya3@gmail.com>
2018-10-08 16:52:04 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 0;
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 2; j < 10; j++) {
+				a = i * j;
+				System.out.print(a + "\t ");
+
+
+			}System.out.println(" ");
+		}
+
+	}
+}
+
